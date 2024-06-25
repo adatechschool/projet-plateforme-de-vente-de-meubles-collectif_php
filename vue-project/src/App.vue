8264345d1e57d0cbbae39df0741a97790b83575f
Anael Toula <anael.toula@gmail.com>
2024-06-25 16:28:41 +0200

    new vite-vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <HelloWorld :sub-title=" subTitle "/>
+  <input type="text" v-model="subTitle"> -->
+  <HelloWorld :user="user"  />
+
+<input type="text" v-model="user.name">
+<input type="text" v-model="user.age">
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import { reactive } from 'vue';
+// const subTitle = ref("Bienvenue, sur Vue.js")
+const user = reactive({
+name: "Anaël",
+age : 21,
+})
+
+// const display = () => {
+//   console.log(user);
+// }
+// display()
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello World</h1>
+  <p>{{ props.user.name }} {{props.user.age}}</p>
+</template>
+
+<script setup>
+const props = defineProps(['user'])
+</script>
+
+<style scoped>
+
+</style>
